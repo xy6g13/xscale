929c99955623bcfff56fee23092e0543468a6784
Aurelien PONTE <aponte@datarmor3.ib0.ice.ifremer.fr>
2018-06-08 10:06:56 +0000

    fix a call to pandas dtype, fix window plot, update setup requirements

--- xscale/_utils.py
+++ xscale/_utils.py
@@ -170,7 +170,7 @@ def get_dx(obj, dim, unit='s'):
 		The resolution of the coordinates associated to the dimension
 	"""
 	x = np.asarray(obj[dim])
-	if pd.core.common.is_datetime64_dtype(x):
+	if pd.core.dtypes.common.is_datetime64_dtype(x):
 		dx = pd.Series(x[1:]) - pd.Series(x[:-1])
 		dx /= np.timedelta64(1, unit)
 	else:
